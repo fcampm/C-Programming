3bf7914d2ce280424797fed6d894003f731430fe
Fabián Camp <fabian.camp@yahoo.com>
2018-02-09 00:41:15 -0600

    Hellow world on C language
    First program in C programming language. Print in the screen "Hello, world :D"

---
+++ C_course/cmake-build-debug/CMakeFiles/C_course.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_course.dir/main.c.obj"
+  "C_course.pdb"
+  "C_course.exe"
+  "C_course.exe.manifest"
+  "libC_course.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C_course.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ C_course/main.c
@@ -0,0 +1,13 @@
+/*
+ * Author: Fabián Camp Mussa.
+ * Github: https://github.com/fcampm
+ * Created: February 9, 2018.
+ * */
+
+#include <stdio.h> // Esta librería principalmente te va a permitir poner cosas en pantalla o pedirle al usuario datos.
+
+// Función main.
+int main() {
+    printf("Hello, World! :D\n"); // El comando printf sirve para poder imprimir en pantalla.
+    return 0;
+}
