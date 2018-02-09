c3707251f7133072dc2224a6cf7159c668524bea
Fabián Camp <fabian.camp@yahoo.com>
2018-02-09 00:42:30 -0600

    ##

--- C_course/main.c
+++ C_course/main.c
@@ -1,7 +1,7 @@
 /*
  * Author: Fabián Camp Mussa.
  * Github: https://github.com/fcampm
- * Created: February 9, 2018.
+ * Created on February 9, 2018.
  * */
 
 #include <stdio.h> // Esta librería principalmente te va a permitir poner cosas en pantalla o pedirle al usuario datos.
