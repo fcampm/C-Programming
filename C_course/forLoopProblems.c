7950a4115644a5007bf15c6081eac130ab0ce8ca
Fabián Camp <fabian.camp@yahoo.com>
2018-05-22 15:52:31 -0500

    for loop problems

--- C_course/forLoopProblems.c
+++ C_course/forLoopProblems.c
@@ -12,7 +12,11 @@ int main(){
 
     //firstProblem();
     //firstProblemSecondSolution();
-    secondProblem();
+    //secondProblem();
+    //thirdProblem();
+    //fourthProblem();
+    //fifthProblem();
+    sixthProblem();
     return 0;
 }
 
@@ -47,12 +51,99 @@ void firstProblemSecondSolution(){
 
 // Compute a program that its output be like shown in Notes 1.
 void secondProblem(){
+    // Variables to use.
     char result[10] = "";
     for(int i = 1; i <= 5; i++){
         strcat(result, "*");
         printf("%s\n", result);
     }
 }
+
+// Determine if a number it's prime or not.
+void thirdProblem(){
+    // Variables to use.
+    int iterator;
+    int number;
+    int counter = 0;
+
+    printf("Compute a number: ");
+    scanf("%i", &number);
+
+    for(iterator = 1; iterator <= number; iterator++){
+        if(number  % iterator == 0){
+            counter++;
+        }
+    }
+
+    if(counter > 2){
+        printf("The number %i is not a prime number", number);
+    }
+    else{
+        printf("The number %i is a prime number", number);
+    }
+}
+
+// Compute the factorial of a number.
+void fourthProblem(){
+    // Variables to use.
+    int number;
+    int iterator;
+    long result = 1;
+
+    printf("Digit a number to obtain the factorial: ");
+    scanf("%i", &number);
+
+    for(iterator = 1; iterator <= number; iterator++){
+        result *= iterator;
+    }
+
+    printf("The result is: %li", result);
+}
+
+// Fibonacci series.
+void fifthProblem(){
+    // Variables to use.
+    int number;
+    int iterator;
+    int x = 0;
+    int y = 1;
+    int z;
+
+    printf("Compute the number of elements: ");
+    scanf("%i", &number);
+
+    printf("1\t");
+
+    for(iterator = 1; iterator < number; iterator++){
+        z = x + y;
+        x = y;
+        y = z;
+
+        printf("%i\t", z);
+    }
+}
+
+// Make a program that prints de sum of all even numbers that exist between 1 to n, and print how many numbers are.
+void sixthProblem(){
+    // Variables to use.
+    int number;
+    int iterator;
+    int counter = 0;
+    int result = 0;
+
+    printf("Digit the superior limit to sum the even numbers: ");
+    scanf("%i", &number);
+
+    for(iterator = 1; iterator <= number; iterator++){
+        if(iterator % 2 == 0){
+            result += iterator;
+            counter++;
+        }
+    }
+
+    printf("The result of the sum is: %i\nTotal of numbers are: %i", result, counter);
+}
+
 
 /* Notes:
  * 1. Output of the second problem:
